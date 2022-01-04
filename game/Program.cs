6b856e800a159915a32f4a59d46296ae344bd126
Opilane TTHK <opilane@tthk.ee>
2022-01-04 09:46:47 +0200

    Add project files.

---
+++ game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] heroes = { "Harry Potter", "Superman", "Luke Skywalker", "Lara Kroft" };
+            string[] villains = { "Volandemort", "Joker", "Venom", "Darth Vader", "Cruella" };
+
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, heroes.Length);
+
+            string randomHero = GetRandomCharacter(heroes);
+            string randomVillain = GetRandomCharacter(villains);
+            Console.WriteLine($"Your random villain is: {randomVillain}");
+            Console.WriteLine($"Your random hero is: {randomHero}");
+            
+        }
+
+        public static string GetRandomCharacter(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomCharacter = someArray[randomIndex];
+            return randomCharacter;
+        }
+    }
+}
