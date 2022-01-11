e22910fd32beba0efbb8b76e9a8490d24c7cb791
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-01-11 10:00:15 +0200

    game new

--- game/Program.cs
+++ game/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.IO;
 
 namespace game
 {
@@ -6,26 +7,36 @@ namespace game
     {
         static void Main(string[] args)
         {
-            string[] heroes = { "Harry Potter", "Superman", "Luke Skywalker", "Lara Kroft" };
-            string[] villains = { "Volandemort", "Joker", "Venom", "Darth Vader", "Cruella" };
-
+            //string[] heroes = { "Harry Potter", "Superman", "Luke Skywalker", "Lara Kroft" };
+            //string[] villains = { "Volandemort", "Joker", "Venom", "Darth Vader", "Cruella" };
+            string folderPath = @"C:\Users\opilane\samples\mygame\";
+            string[] heroes = GetDataFromFile(folderPath + "heroes.txt");
+            string[] villains = GetDataFromFile(folderPath + "villains.txt");
+            string[] weapons = GetDataFromFile(folderPath + "weapons.txt");
 
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, heroes.Length);
 
-            string randomHero = GetRandomCharacter(heroes);
-            string randomVillain = GetRandomCharacter(villains);
+            string randomHero = GetRandomElement(heroes);
+            string randomVillain = GetRandomElement(villains);
+            string heroWeapon = GetRandomElement(weapons);
+            string villainWeapon = GetRandomElement(weapons);
             Console.WriteLine($"Your random villain is: {randomVillain}");
             Console.WriteLine($"Your random hero is: {randomHero}");
-            
+            Console.WriteLine($"{randomHero} with {heroWeapon} will fight {randomVillain} with {villainWeapon}");
         }
 
-        public static string GetRandomCharacter(string[] someArray)
+        public static string GetRandomElement(string[] someArray)
         {
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, someArray.Length);
             string randomCharacter = someArray[randomIndex];
             return randomCharacter;
+        }
+        public static string[] GetDataFromFile(string filePath)
+        {
+            string[] dataFromfile = File.ReadAllLines(filePath);
+            return dataFromfile;
         }
     }
 }
