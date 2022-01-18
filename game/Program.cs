a3417b17152c6238af980749c75e111c64be463a
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2022-01-18 09:41:26 +0200

    Update Program.cs
    End

--- game/Program.cs
+++ game/Program.cs
@@ -13,6 +13,7 @@ namespace game
             string[] heroes = GetDataFromFile(folderPath + "heroes.txt");
             string[] villains = GetDataFromFile(folderPath + "villains.txt");
             string[] weapons = GetDataFromFile(folderPath + "weapons.txt");
+            string[] armor = GetDataFromFile(folderPath + "armor.txt");
 
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, heroes.Length);
@@ -21,9 +22,38 @@ namespace game
             string randomVillain = GetRandomElement(villains);
             string heroWeapon = GetRandomElement(weapons);
             string villainWeapon = GetRandomElement(weapons);
-            Console.WriteLine($"Your random villain is: {randomVillain}");
-            Console.WriteLine($"Your random hero is: {randomHero}");
+            string heroArmor = GetRandomElement(armor);
+            string villainArmor = GetRandomElement(armor);
+
+
+            int heroHP = GenerateHP(heroArmor);
+            int villainHP = GenerateHP(villainArmor);
+
+            Console.WriteLine($"Your random villain is: {randomVillain} in {villainArmor}");
+            Console.WriteLine($"Your random hero is: {randomHero} in {heroArmor}");
             Console.WriteLine($"{randomHero} with {heroWeapon} will fight {randomVillain} with {villainWeapon}");
+            Console.WriteLine($"{randomVillain} HP: {villainHP}");
+            Console.WriteLine($"{randomHero} HP: {heroHP}");
+
+            while(heroHP >= 0 && villainHP >= 0 )
+            {
+                heroHP = heroHP - Hit(randomVillain, villainWeapon);
+                villainHP = villainHP - Hit(randomHero, heroWeapon);
+            }
+
+            if (heroHP > villainHP)
+            {
+                Console.WriteLine($"{randomHero} saves the day!");
+            }
+            else if(heroHP<villainHP)
+            {
+                Console.WriteLine("Dark Side wins!");
+            }
+            else
+            {
+                Console.WriteLine($"Someday {randomHero} shall fight {randomVillain} again!");
+            }
+
         }
 
         public static string GetRandomElement(string[] someArray)
@@ -38,5 +68,27 @@ namespace game
             string[] dataFromfile = File.ReadAllLines(filePath);
             return dataFromfile;
         }
+
+        public static int GenerateHP(string armor)
+        {
+            int characterHP = armor.Length;
+            return characterHP;
+        }
+
+        public static int Hit(string character, string weapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, weapon.Length - 2);
+            Console.WriteLine($"{character} hit {strike}!");
+
+            if(strike== weapon.Length - 2)
+            {
+                Console.WriteLine($"Bad luck! {character} missed the target!");
+            }
+            return strike;
+
+        }
+
+        
     }
 }
